ec2389134bca52564a1549a479ca35e77e51b48e
Chrisborer <chris.borer@gmail.com>
2022-04-09 13:01:54 -0500

    planner is complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,43 @@
-//Daily Calendar
-//created by Christopher Borer
+$(document).ready(function () {
+    //display current day
+    $('#currentDay').text(moment().format('dddd, MMMM Do'));
+  //Pulling the values on click
+  $(".saveBtn").on("click", function () {
+    let desc = $(this).siblings(".description").val();
+    let timeStamp = $(this).parent().attr("id");
+    //save to local storage
+    localStorage.setItem(timeStamp, desc);
+  });
 
-//Element variables
-var eventEl = document.getElementById("event-rows");
-var iconEl = '<img src="./assets/images/calendar-plus-solid.svg"></img>';
+  var colorCode = function () {
+    let currentHour = moment().hours();
+    $(".time-block").each(function () {
+      let blockHour = parseInt($(this).attr("id").split("-")[1]);
+      //Compare current hour to block hour
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else if (blockHour > currentHour) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  };
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
-//Class variables
-var divClass1 = "col-2";
-var divClass10 = "col-8";
+  var interval = setInterval(colorCode, 20000);
 
-//Date varaibles
-var hoursInDay = 24;
-var now = moment();
-var today = now.format("dddd MMMM Do YYYY");
-var endOfDay = now.endOf("day");
-
-//Data
-var schedule = {
-    hour05: "learn java script"
-};
-
-//Display the current date in the header
-var displayCurrentDate = function (currentDate) {
-  $("#currentDay").html(currentDate);
-};
-
-//Get the start of day hour
-var getStartOfday = function (currentHour) {
-  const startOfDay = currentHour.startOf("day");
-  //console.log('start of day  ' + startOfDay.format("HH"));
-
-  //calss getschedule to pull from local storage
-  getSchedule();
-  //Call add events to update hours on main page
-  addEvents(startOfDay);
-};
-
-//Build out the calendar events.
-var addEvents = function (startOfDay) {
-  //console.log(startOfDay.format('HH'));
-  let newHour = parseInt(startOfDay.format("HH"));
-
-  for (let i = 1; newHour < hoursInDay; newHour++) {
-    startOfDay.add(i, "hour");
-    let row = document.createElement("div");
-    row.className = "row";
-    let timeStamp = document.createElement("div");
-    timeStamp.className = divClass1;
-    let toDo = document.createElement("textarea");
-    let timeOfDay = " present";
-    if(startOfDay < now.format("HH")) {
-        timeOfDay = " past";
-    }
-    else if(startOfDay > now.add(1, 'hour').format('HH')) {
-        timeOfDay = " future";
-    }
-    toDo.className = divClass10 + timeOfDay;
-    let id = "hour" + startOfDay.format("HH");
-    toDo.id = id;
-    let editTask = document.createElement("button");
-    editTask.className = divClass1 + " saveBtn ";
-    
-    timeStamp.textContent = startOfDay.format("LT");
-    toDo.value = schedule[id] || "";
-    editTask.innerHTML = iconEl;
-
-    row.append(timeStamp, toDo, editTask);
-    eventEl.append(row);
-    editTask.addEventListener("click", editEvents);
-  }
-};
-
-var editEvents = function(event) {
-    let row = event.target.closest(".row");
-    let textarea = row.querySelector("textarea");
-    let id = textarea.id;
-    let task = textarea.value;
-    schedule[id] = task;
-    saveSchedule();
-}
-
-var saveSchedule = function() {
-    localStorage.setItem("schedule", JSON.stringify(schedule));
-}
-
-var getSchedule = function() {
-    let data = localStorage.getItem("schedule");
-    if(data) {
-        schedule = JSON.parse(data);
-    }
-    else {
-        schedule = {};
-    }
-}
-
-//function calls
-getStartOfday(now);
-displayCurrentDate(today);
-
+  //function calls
+  colorCode();
+});
